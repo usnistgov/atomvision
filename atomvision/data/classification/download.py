c6717dc7d7be2ef1444a3079fa53810607c01127
knc6 <writetokamal.1989@gmail.com>
2021-10-12 09:51:21 -0400

    Adding example data.

---
+++ atomvision/data/classification/download.py
@@ -0,0 +1,82 @@
+"""Download data from figshare."""
+import requests
+import zipfile
+import os
+from tqdm import tqdm
+
+
+def get_db_info():
+    """Get DB info."""
+    db_info = {
+        # https://doi.org/10.6084/m9.figshare.16788268
+        "stem_c2db": [
+            "https://figshare.com/ndownloader/files/31054516",
+            "stem_c2db.zip",
+            "Obtaining C2DB 2D STEM image dataset 3.5k ...",
+            "https://iopscience.iop.org/article/10.1088/2053-1583/aacfc1",
+        ],
+        "stem_jv2d": [
+            "https://figshare.com/ndownloader/files/31054519",
+            "stem_jv2d.zip",
+            "Obtaining JARVIS-DFT 2D STEM image dataset 1.1k ...",
+            "https://www.nature.com/articles/s41524-020-00440-1",
+        ],
+        "stm_jv2d": [
+            "https://figshare.com/ndownloader/files/31054525",
+            "stm_jv2d.zip",
+            "Obtaining JARVIS-DFT 2D STM image dataset 1.4k ...",
+            "https://www.nature.com/articles/s41524-020-00440-1",
+        ],
+        "stm_tdmp": [
+            "",
+            "stm_jv2d.zip",
+            "Obtaining 2DMatPefia 2D STM image dataset 6k ...",
+            "https://www.nature.com/articles/s41597-019-0097-3",
+        ],
+    }
+    return db_info
+
+
+def data(
+    dataset="stem_jv2d",
+    my_path=None,
+):
+    """Get data with progress bar."""
+    db_info = get_db_info()
+    if dataset not in list(db_info.keys()):
+        raise ValueError("Check DB name options.")
+
+    zfile = dataset + ".zip"
+    url = db_info[dataset][0]
+
+    path = str(os.path.join(os.path.dirname(__file__), zfile))
+    # path = str(os.path.join(os.path.dirname(__file__), js_tag))
+    if not os.path.isfile(path):
+        # zfile = str(os.path.join(os.path.dirname(__file__), "tmp.zip"))
+        response = requests.get(url, stream=True)
+        total_size_in_bytes = int(response.headers.get("content-length", 0))
+        block_size = 1024  # 1 Kibibyte
+        progress_bar = tqdm(
+            total=total_size_in_bytes, unit="iB", unit_scale=True
+        )
+        with open(path, "wb") as file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+        progress_bar.close()
+        # f = open(zfile, "wb")
+        # f.write(r.content)
+        # f.close()
+    if my_path is None:
+        my_path = os.path.abspath(
+            str(os.path.join(os.path.dirname(__file__), "."))
+        )
+        print("Found extract_path as None.")
+    print("Loading and extracting the zipfile...")
+    final_path = os.path.join(my_path, dataset)
+    if not os.path.exists(final_path):
+        info = zipfile.ZipFile(path).extractall(my_path)
+    print("Files are be kept at:", final_path)
+    print("Process completed.")
+
+    return final_path
---
+++ atomvision/data/classification/stem_jv2d/generate_data.py
@@ -0,0 +1,87 @@
+from jarvis.core.atoms import crop_square
+import matplotlib.pyplot as plt
+import os, glob
+from jarvis.analysis.stem.convolution_apprx import STEMConv
+from jarvis.db.figshare import data, get_jid_data
+import matplotlib.pyplot as plt
+from jarvis.core.atoms import Atoms, ase_to_atoms, get_supercell_dims
+from jarvis.core.lattice import get_2d_lattice
+from sklearn.model_selection import train_test_split
+from collections import defaultdict
+
+plt.switch_backend("agg")
+
+
+dft_2d = data("dft_2d")
+for i in dft_2d:
+    a = Atoms.from_dict(i["atoms"])
+    jid = i["jid"]
+    dims = get_supercell_dims(atoms=a, enforce_c_size=50)
+    s = a.make_supercell_matrix(dims)
+    c = crop_square(s, csize=20)
+    p = STEMConv(atoms=c, output_size=[200, 200]).simulate_surface()
+    plt.imshow(p[50:150, 50:150], interpolation="gaussian", cmap="gray")
+    plt.tight_layout()
+    plt.axis("off")
+    filename = jid + ".jpg"
+    plt.savefig(filename)
+    plt.close()
+
+
+lat_dict = defaultdict()
+for i in dft_2d:
+    lat_dict[i["jid"]] = get_2d_lattice(i["atoms"])[1]
+lat_dict_data = defaultdict(list)
+
+for i in glob.glob("JVASP-*.jpg"):
+    jid = i.split(".jpg")[0]
+    lat_type = lat_dict[jid]
+    lat_dict_data[lat_type].append(i)
+train_folder = "train_folder"
+test_folder = "test_folder"
+test_size = 0.2
+if not os.path.exists(train_folder):
+    os.makedirs(train_folder)
+if not os.path.exists(test_folder):
+    os.makedirs(test_folder)
+
+for i, j in lat_dict_data.items():
+    splits = train_test_split(list(j), test_size=test_size)
+    train_samples = splits[0]
+    cwd = os.getcwd()
+    fold_name = os.path.join(str(train_folder), str(i))
+    if not os.path.exists(fold_name):
+        os.makedirs(fold_name)
+    for k in train_samples:
+        cmd = "mv " + k + " " + train_folder + "/" + str(i)
+        os.system(cmd)
+    os.chdir(cwd)
+    test_samples = splits[1]
+    fold_name = os.path.join(str(test_folder), str(i))
+    if not os.path.exists(fold_name):
+        os.makedirs(fold_name)
+    for k in test_samples:
+        cmd = "mv " + k + " " + test_folder + "/" + str(i)
+        os.system(cmd)
+
+
+"""
+for ii, i in enumerate(dft_2d):
+    a = Atoms.from_dict(i["atoms"])
+    lat_type=str(get_2d_lattice(a.to_dict())[1])
+    if not os.path.exists(lat_type):
+        os.makedirs(lat_type)
+    jid = i["jid"]
+    print(ii,jid,lat_type)
+    dims = get_supercell_dims(atoms=a, enforce_c_size=50)
+    s = a.make_supercell_matrix(dims)
+    c = crop_square(s, csize=20)
+    p = STEMConv(atoms=c, output_size=[200, 200]).simulate_surface()
+    plt.imshow(p[50:150, 50:150], interpolation="gaussian", cmap="gray")
+    plt.tight_layout()
+    plt.axis("off")
+    filename = lat_type+"/STEM-" + jid + ".jpg"
+    plt.savefig(filename)
+    plt.close()
+    # break
+"""
