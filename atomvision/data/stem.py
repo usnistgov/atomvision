83b5eb1db360dfb664ad8d163c2caa5298748c52
Brian DeCost <brian.decost@nist.gov>
2022-02-11 10:50:10 -0500

    implement atomistic graph dataloader based on predicted atomistic masks.

--- atomvision/data/stem.py
+++ atomvision/data/stem.py
@@ -15,6 +15,10 @@ from atomvision.data.stemconv import STEMConv
 from collections.abc import Callable
 from typing import Optional, List, Dict, Any
 
+from skimage import measure
+from scipy.spatial import KDTree
+import networkx as nx
+
 
 LABEL_MODES = {"delta", "radius"}
 
@@ -103,4 +107,96 @@ class Jarvis2dSTEMDataset:
         sample = {"image": image, "label": torch.FloatTensor(label > 0), "id": row.jid}
 
         # sample = {"image": image, "label": label, "coords": pos, "id": row.jid}
+        return sample
+
+
+def atom_mask_to_graph(label, image, cutoff=40):
+    """Construct attributed atomistic graph from foreground mask
+
+    Performs connected component analysis on label image
+    Computes region properties (centroids, radius, mean intensity)
+    Constructs a radius graph of atoms within `cutoff` (px)
+    """
+    g = nx.Graph()
+
+    # connected component analysis
+    rlab = measure.label(label)
+
+    # per-atom-detection properties for node attributes
+    props = pd.DataFrame(
+        measure.regionprops_table(
+            rlab,
+            intensity_image=image / image.max(),
+            properties=[
+                "label",
+                "centroid",
+                "equivalent_diameter",
+                "min_intensity",
+                "mean_intensity",
+                "max_intensity",
+            ],
+        )
+    )
+
+    # add nodes with attributes to graph
+    for id, row in props.iterrows():
+        pos = np.array([row["centroid-1"], row["centroid-0"], 0])
+        g.add_node(
+            id, pos=pos, intensity=row.mean_intensity, r=row.equivalent_diameter / 2
+        )
+
+    # construct radius graph edges via kd-tree
+    points = props.loc[:, ("centroid-1", "centroid-0")].values
+    nbrs = KDTree(points)
+    g.add_edges_from(nbrs.query_pairs(cutoff))
+
+    return g, props
+
+
+class Jarvis2dSTEMGraphDataset(Jarvis2dSTEMDataset):
+    """Simulated STEM dataset (jarvis dft_2d): graph encoding"""
+
+    def __init__(
+        self,
+        px_scale: float = 0.1,
+        label_mode: str = "delta",
+        image_data: Optional[List[Dict[str, Any]]] = None,
+        to_tensor: Optional[Callable] = None,
+        pixel_classifier=None,
+        debug=False,
+    ):
+        """Simulated STEM dataset, jarvis-2d data
+
+        px_scale: pixel size in angstroms
+        label_mode: `delta` or `radius`, controls atom localization mask style
+
+        pixel_classifier: pretrained model for labeling pixels atoms/background
+        debug: use ground truth label annotations
+
+        Running the pixel classifier like this in the dataloader is not the most efficient
+        It might be viable to put this inside a closure used for a dataloader collate_fn
+        This would assemble the full batch, run the pixel classifier, and then construct
+        the atomistic graphs. Hopefully this is all done during DataLoader prefetch still.
+
+        Depending on the quality of the label predictions, this could potentially need
+        label smoothing as well.
+        """
+        super().__init__(
+            px_scale=px_scale, label_mode=label_mode, image_data=image_data
+        )
+        self.pixel_classifier = pixel_classifier
+        self.debug = debug
+
+    def __getitem__(self, idx):
+        """Sample: image, label mask, atomic coords, numbers, structure ids."""
+        sample = super().__getitem__(idx)
+
+        if self.debug:
+            predicted_label = sample["label"]
+        else:
+            predicted_label = self.pixel_classifier(sample["image"])
+
+        g, props = atom_mask_to_graph(predicted_label, sample["image"])
+        sample["g"] = g
+
         return sample
